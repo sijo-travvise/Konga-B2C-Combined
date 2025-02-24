d9cbf2bc657eac1de6b03234505ff673d15a1e79
anagha-travvise <anagha.mj@travvise.com>
2025-02-24 12:49:22 +0300

    redirect  affiliate done

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ForNotForComponent } from './for-not-for/for-not-for.component';
 import { PreLoaderComponent } from './pre-loader/pre-loader.component';
+import { AffliateComponent } from './affliate/affliate.component';
 
 
 const routes: Routes = [
   // {path:'preloader', component:PreLoaderComponent},
   {path:'404', component:ForNotForComponent},
+  {path:'affiliate', component:AffliateComponent},
 ];
 
 @NgModule({
