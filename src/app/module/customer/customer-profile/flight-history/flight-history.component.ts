67e7a4285bd27510e7b7508ebc827565a0e63ec7
anagha-travvise <anagha.mj@travvise.com>
2025-03-11 13:13:56 +0300

    added loading

--- src/app/module/customer/customer-profile/flight-history/flight-history.component.ts
+++ src/app/module/customer/customer-profile/flight-history/flight-history.component.ts
@@ -32,9 +32,11 @@ export class FlightHistoryComponent {
     this.bookingDetailsTab = 'out';
   }
   toggleAccordion(FlightTransactions_ID: string = '0', Transaction: any): void {
+    this.isLoading = true;
     if (FlightTransactions_ID === this.bookingDetailsTab) {
       this.bookingDetailsTab = '0'; // Close the tab if it's already open
       return;
+      this.isLoading = false;
     }
   
     this.bookingDetailsTab = FlightTransactions_ID; // Open the tab
@@ -97,7 +99,7 @@ export class FlightHistoryComponent {
       payment:this.parseJSON(Transaction.flightTransactionsPaymentGateway_JSON)
     };
     
-    console.log('AllFlightDetails', this.AllFlightDetails);
+    //console.log('AllFlightDetails', this.AllFlightDetails);
     this.isLoading = false;
   }
 }
